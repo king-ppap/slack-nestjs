0e172b968a815818530b16b4be1cc7011ba826cb
King Ponpawit Paoseng <kingponpawit@gmail.com>
2022-01-21 16:55:00 +0700

    Update end flow aprove

--- src/slack/quotation/views/approve.view.ts
+++ src/slack/quotation/views/approve.view.ts
@@ -1,11 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
-import {
-  AllMiddlewareArgs,
-  BlockAction,
-  GenericMessageEvent,
-  SlackActionMiddlewareArgs,
-  SlackViewMiddlewareArgs,
-} from '@slack/bolt';
+import { AllMiddlewareArgs, GenericMessageEvent, SlackViewMiddlewareArgs } from '@slack/bolt';
 import { BotFunction, BotFunctionType } from '../../bot/utils/bot.interface';
 
 @Injectable()
@@ -35,20 +29,40 @@ export class ApproveConfirmView implements BotFunction {
   }: SlackViewMiddlewareArgs & AllMiddlewareArgs) {
     // const message = <GenericMessageEvent>body.message;
     this.logger.debug(body.view.title);
+    const ts = body.view.blocks[2].text;
+    this.logger.debug(ts, process.env.START_CHANEL_ID);
+    try {
+      // Call the conversations.history method using the built-in WebClient
+      const result = await client.conversations.history({
+        channel: process.env.START_CHANEL_ID,
+        // In a more realistic app, you may store ts data in a db
+        latest: ts,
+        // Limit results
+        inclusive: true,
+        limit: 1,
+      });
 
+      // There should only be one result (stored in the zeroth index)
+      const message = <GenericMessageEvent>(<unknown>result.messages[0]);
+      // Print message text
+      this.logger.debug(message.text);
 
-    // await client.chat.update({
-    //   channel: process.env.
-    //   attachments: [
-    //     {
-    //       color: '#67C23A',
-    //       blocks: [
-    //         ...message.attachments[0].blocks.slice(0, 3),
-    //         ...message.attachments[0].blocks.slice(4),
-    //       ],
-    //     },
-    //   ],
-    // });
+      await client.chat.update({
+        channel: process.env.START_CHANEL_ID,
+        ts: ts,
+        attachments: [
+          {
+            color: '#67C23A',
+            blocks: [
+              ...message.attachments[0].blocks.slice(0, 3),
+              ...message.attachments[0].blocks.slice(4),
+            ],
+          },
+        ],
+      });
+    } catch (error) {
+      this.logger.error(error);
+    }
 
     await ack();
   }
