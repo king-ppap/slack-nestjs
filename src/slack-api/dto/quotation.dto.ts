17c06aa161969afc1e7a21525668f414934ec551
King Ponpawit Paoseng <kingponpawit@gmail.com>
2022-01-25 11:32:09 +0700

    ๊Update SendMessageQuotationDto

--- src/slack-api/dto/quotation.dto.ts
+++ src/slack-api/dto/quotation.dto.ts
@@ -1,8 +1,37 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Matches } from 'class-validator';
 
+class ContactInterfaceDto {
+  @ApiProperty()
+  restaurant_name: string;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  company: string;
+
+  @ApiProperty()
+  phone: string;
+
+  @ApiProperty()
+  email: string;
+}
+
 export class SendMessageQuotationDto {
   @ApiProperty()
+  qt_id: number;
+
+  @ApiProperty()
   @Matches(/^QT\d{11}$/)
-  qt_id: string;
+  qt_number: string;
+
+  @ApiProperty()
+  contact: ContactInterfaceDto;
+
+  @ApiProperty()
+  price: number;
+
+  @ApiProperty()
+  sale: string;
 }
